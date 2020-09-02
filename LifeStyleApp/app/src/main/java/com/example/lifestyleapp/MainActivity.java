b9bd454e9aadde58d5d90f3e56967baca2ed91da
williamrdent <williamrdent@gmail.com>
2020-09-01 22:01:24 -0600

    User Profile button function

--- LifeStyleApp/app/src/main/java/com/example/lifestyleapp/MainActivity.java
+++ LifeStyleApp/app/src/main/java/com/example/lifestyleapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lifestyleapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -7,6 +8,7 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.View;
 
@@ -19,17 +21,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+    }
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    public void viewUserProfile(View view){
+        Intent intent = new Intent(this,UserProfile.class);
+        startActivity(intent);
     }
 
     @Override
