a01aa8ca7a94542c60260f8025c446ee0e42d675
williamrdent <williamrdent@gmail.com>
2020-09-02 08:27:56 -0600

    Weather activity changed to fullscreen

--- LifeStyleApp/app/src/main/java/com/example/lifestyleapp/UserProfile.java
+++ LifeStyleApp/app/src/main/java/com/example/lifestyleapp/UserProfile.java
@@ -11,4 +11,8 @@ public class UserProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
     }
+
+    public void uploadProfilePicture(){
+
+    }
 }
