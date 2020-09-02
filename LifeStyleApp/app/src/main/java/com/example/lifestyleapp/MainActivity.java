271caffac72b513f5071ad9849335b0aa0f651fb
williamrdent <williamrdent@gmail.com>
2020-09-02 07:50:17 -0600

    Scrollable weather activity added

--- LifeStyleApp/app/src/main/java/com/example/lifestyleapp/MainActivity.java
+++ LifeStyleApp/app/src/main/java/com/example/lifestyleapp/MainActivity.java
@@ -28,6 +28,11 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void viewWeather(View view){
+        Intent intent = new Intent(this,Weather.class);
+        startActivity(intent);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
