2111129340ad98a53b60c046df52002f63a389ff
williamrdent <williamrdent@gmail.com>
2020-09-08 10:52:40 -0600

    Garbage before revert

--- LifeStyleApp/app/src/main/java/com/example/lifestyleapp/MainActivity.java
+++ LifeStyleApp/app/src/main/java/com/example/lifestyleapp/MainActivity.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.StrictMode;
 import android.view.View;
@@ -19,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     public void viewUserProfile(View view){
