96aebd8e2d6060331e3784b52eb925b2bd019547
Evan George <evangeorge1994@gmail.com>
2020-09-08 21:58:47 -0600

    Resolve conflicts

--- LifeStyleApp/app/src/main/java/com/example/lifestyleapp/LoginActivity.java
+++ LifeStyleApp/app/src/main/java/com/example/lifestyleapp/LoginActivity.java
@@ -32,7 +32,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 Context.MODE_PRIVATE);
 
 //         The following line is for testing purposes. It forces credentials.
-        pref.edit().putBoolean(LOGIN_KEY, false).apply();
+        pref.edit().putBoolean(LOGIN_KEY, true).apply();
 
         if (pref.getBoolean(LOGIN_KEY, false)) {
             startActivity(new Intent(this, MainDrawerActivity.class));
