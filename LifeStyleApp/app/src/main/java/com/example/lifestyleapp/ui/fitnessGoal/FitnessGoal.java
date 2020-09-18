e0ab2cf30ac4d43cb806ebb176a851368b2fe34f
williamrdent <williamrdent@gmail.com>
2020-09-18 10:08:40 -0600

    Fitness goal Fragment Added

---
+++ LifeStyleApp/app/src/main/java/com/example/lifestyleapp/ui/fitnessGoal/FitnessGoal.java
@@ -0,0 +1,23 @@
+package com.example.lifestyleapp.ui.fitnessGoal;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.lifestyleapp.R;
+
+public class FitnessGoal extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_fitness_goal, container, false);
+    }
+}
