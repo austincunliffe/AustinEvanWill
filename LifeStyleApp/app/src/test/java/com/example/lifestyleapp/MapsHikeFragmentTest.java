e56095e3e0e3d33c2edd9fa6c14fddab7989c1d7
austincunliffe <austinmcunliffe@gmail.com>
2020-09-19 11:05:08 -0700

    Added some unit and instrument tests.

---
+++ LifeStyleApp/app/src/test/java/com/example/lifestyleapp/MapsHikeFragmentTest.java
@@ -0,0 +1,14 @@
+package com.example.lifestyleapp;
+
+import com.example.lifestyleapp.ui.mapHikes.MapsHikeFragment;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MapsHikeFragmentTest {
+
+    @Test
+    public void getNearbyHikesTest1() {
+    }
+}
