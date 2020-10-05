27b74635c8270bb870959d62616e60a7cf719dc8
williamrdent <williamrdent@gmail.com>
2020-10-05 12:59:26 -0600

    BMI Refactored to ViewModel

---
+++ LifeStyleApp/app/src/main/java/com/example/lifestyleapp/ui/bmi/BMIViewModel.java
@@ -0,0 +1,62 @@
+package com.example.lifestyleapp.ui.bmi;
+
+import android.annotation.SuppressLint;
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+public class BMIViewModel extends ViewModel {
+
+    private Application application;
+
+    String userBMIString;
+
+
+    BMIViewModel(Application application) {
+        this.application = application;
+        loadUserData();
+    }
+
+    void loadUserData() {
+
+        SharedPreferences pref = application.getSharedPreferences("com.example.lifestyleapp",
+                Context.MODE_PRIVATE);
+
+        int mHeightInt = pref.getInt("height", 0);
+        int mWeightInt = pref.getInt("weight", 0);
+
+        double userBMI = calculateBMI(mWeightInt, mHeightInt);
+
+        userBMIString = formatBMI(userBMI);
+        System.out.println(userBMIString);
+    }
+
+
+    public static double calculateBMI(int userWeight, int userHeight) {
+        // this is the formula for calculating BMI in imperial units
+        double bmi = (703 * (userWeight / Math.pow(userHeight, 2)));
+        return bmi;
+    }
+
+    public String formatBMI(Double userBMI) {
+        //rounding the bmi to one 3 significant digits
+        BigDecimal bd = new BigDecimal(userBMI);
+        bd = bd.round(new MathContext(3));
+        double roundedUserBMI = bd.doubleValue();
+        return Double.toString(roundedUserBMI);
+    }
+
+    public String getBMI() {
+        return userBMIString;
+    }
+}
+
+
