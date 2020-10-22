ebe222ea519af5c581fdbe4e9df2bde839c1c9bc
williamrdent <williamrdent@gmail.com>
2020-10-22 12:47:39 -0600

    Step count feature structure added

---
+++ LifeStyleApp/app/src/main/java/com/example/lifestyleapp/ui/stepCount/StepCountFragment.java
@@ -0,0 +1,38 @@
+package com.example.lifestyleapp.ui.stepCount;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.lifestyleapp.R;
+
+public class StepCountFragment extends Fragment {
+
+    private StepCountViewModel mViewModel;
+
+    public static StepCountFragment newInstance() {
+        return new StepCountFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.step_count_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(StepCountViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ LifeStyleApp/app/src/main/java/com/example/lifestyleapp/ui/stepCount/StepCountViewModel.java
@@ -0,0 +1,7 @@
+package com.example.lifestyleapp.ui.stepCount;
+
+import androidx.lifecycle.ViewModel;
+
+public class StepCountViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
