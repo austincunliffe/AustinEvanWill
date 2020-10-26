d472edd0f727f9efb97a994c363c5b648b9d4def
austincunliffe <austinmcunliffe@gmail.com>
2020-10-26 08:35:18 -0600

    cleanup

--- LifeStyleApp/app/src/main/java/com/example/lifestyleapp/login/LoginActivity.java
+++ LifeStyleApp/app/src/main/java/com/example/lifestyleapp/login/LoginActivity.java
@@ -25,8 +25,6 @@ import com.example.lifestyleapp.ui.userProfile.UserProfileViewModel;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
-//    private static final String LOGIN_KEY = "LOGIN_KEY";
-
     private LoginActivityViewModel LoginActivityViewModel;
     UserDao userDao;
 
@@ -42,17 +40,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         et_uname = findViewById(R.id.username);
         et_pw = findViewById(R.id.password);
 
-//        SharedPreferences pref = getSharedPreferences("com.example.lifestyleapp",
-//                Context.MODE_PRIVATE);
-
-//         The following line is for testing purposes. It forces credentials.
-//        pref.edit().putBoolean(LOGIN_KEY, false).apply();
-
-//        if (pref.getBoolean(LOGIN_KEY, false)) {
-//            startActivity(new Intent(this, MainDrawerActivity.class));
-//            finish();
-//        }
-
         // Create the view model
         LoginActivityViewModel = ViewModelProviders.of(this).get(LoginActivityViewModel.class);
 
@@ -64,35 +51,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         Button bt_signUp = findViewById(R.id.signUp);
         bt_signUp.setOnClickListener(this);
     }
-
-//    @Override
-//    public void onClick(View v) {
-//        switch(v.getId()){
-//            case R.id.signUp:{
-//                this.startActivity(new Intent(this, CreateAccountActivity.class));
-//                break;
-//            }
-//
-//            case R.id.login:{
-//                SharedPreferences pref = getSharedPreferences("com.example.lifestyleapp",
-//                        Context.MODE_PRIVATE);
-//
-//                un = et_uname.getText().toString();
-//                user_pw = et_pw.getText().toString();
-//
-//                String username = pref.getString("username", null);
-//                String pw = pref.getString("password", null);
-//
-//                if (username == null || pw == null || (!username.matches(un)) || (!user_pw.matches(pw))) {
-//                    Toast.makeText(this, "Invalid login.", Toast.LENGTH_SHORT).show();
-//                } else {
-//                    pref.edit().putBoolean(LOGIN_KEY, true).apply();
-//                    startActivity(new Intent(this, MainDrawerActivity.class));
-//                }
-//                break;
-//            }
-//        }
-//    }
 
     @Override
     public void onClick(View v) {
@@ -122,7 +80,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             if(verified==true) {
                 successfulLogin();
             } else {
-//                unsuccessfulLogin();;
+                unsuccessfulLogin();;
             }
         }
     };
